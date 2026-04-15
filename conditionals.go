2f370bcafb18fc3c1aae07205efa1d0ba80d455f
Cedric <crubbertire@rogers.com>
2026-04-14 17:13:15 -0700

    switch cases

--- conditionals.go
+++ conditionals.go
@@ -20,12 +20,38 @@ func conditionals() {
 
 func initialStatement() {
 	email := "hi@me.com"
+
+	// We can use an initial statement in the `if` statement.
+	// This statement can be used in the scope of the `if` body
+	// This allows for global variables to exist without interconnection
 	if length := utf8.RuneCountInString(email); length < 10 {
 		fmt.Printf("Email must be at least 10 characters , it is %d\n", length)
 	}
 }
 
+// Billing cost function for the switchStatement()
+func billingCost(plan string) float64 {
+	switch plan {
+	case "basic":
+		return 10.0
+	case "pro":
+		return 20.0
+	case "enterprise":
+		return 50.0
+	default:
+		return 0.0
+	}
+}
+
+func switchStatement() {
+	plan := "free"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+	plan = "enterprise"
+	fmt.Printf("The cost for a %s plan is $%.2f\n", plan, billingCost(plan))
+}
+
 func main() {
 	// conditionals()
-	initialStatement()
+	// initialStatement()
+	switchStatement()
 }
